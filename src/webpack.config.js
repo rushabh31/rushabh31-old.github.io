8064688e45a7819752f6206fa3590c2af152968a
rushabh31 <rushabh.patel5@gmail.com>
2020-07-03 15:23:42 -0400

    update build

---
+++ src/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+    plugins: [
+        // other plugins,
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+    ]
+  },
+};
