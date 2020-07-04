0762fb34d4fde8e525136a9fb90e60b2e8f7317b
rushabh31 <rushabh.patel5@gmail.com>
2020-07-03 22:11:07 -0400

    update build

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -7,10 +7,21 @@ import { Fade } from "react-reveal";
 export default function Greeting() {
   return (
     <Fade bottom duration={1000} distance="40px">
+    <section id="feature1">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-6">
+        </div>
+      </div>
+    </div>
+    </section>
+
       <div className="greet-main" id="greeting">
+
         <div className="greeting-main">
           <div className="greeting-text-div">
             <div>
+
               <h2> {greeting.title} </h2>
               <p className="greeting-text-p subTitle">{greeting.subTitle}</p>
               <SocialMedia />
