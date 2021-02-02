517d42f0515a9ca64d7c71b2c0af5406181fa7da
rushabh31 <rushabh.patel5@gmail.com>
2021-02-02 09:57:03 -0500

    fixing screen width

--- src/components/Loader/LoaderLogo.js
+++ src/components/Loader/LoaderLogo.js
@@ -5,9 +5,11 @@ class LogoLoader extends React.Component {
   render() {
     return (
     
-      <img src={require("../../assests/images/splash_logo_2.gif")} width = "500px"></img>
+      //<img src={require("../../assests/images/splash_logo_2.gif")} width = "500px"></img>
       
-
+      <video id = "video-splash" autoPlay loop muted>
+            <source src={require("../../assests/images/splash_screen.mp4")} type='video/mp4' />
+      </video>
       
     );
   }
